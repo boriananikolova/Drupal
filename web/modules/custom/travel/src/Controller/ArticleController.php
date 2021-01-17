dd743f533611066e3a2e51ab11309bd5ae66b0b1
boriananikolova <boriana.nikolova7@gmail.com>
2021-01-18 01:07:51 +0200

    Module

---
+++ web/modules/custom/travel/src/Controller/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\travel\Controller;
+
+class ArticleController {
+
+    public function page(){
+
+        $items = array(
+            array('name' => 'Article one'),
+            array('name' => 'Article two'),
+            array('name' => 'Article three'),
+            array('name' => 'Article four'),         
+        );
+
+        return array(           
+            '#theme' => 'article_list',
+            '#items' => $items,
+            '#title' => 'Our article theme'
+        );
+    }
+}
